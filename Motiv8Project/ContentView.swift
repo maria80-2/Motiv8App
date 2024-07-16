dad42db1bdb7708caa100b2fb8d1287759e65504
svnjanav <sanjanavkanth@gmail.com>
2024-07-15 22:34:12 -0700

    Initial Commit

---
+++ Motiv8Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Motiv8Project
+//
+//  Created by Sanjana V on 7/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Motiv8Project/Motiv8ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Motiv8ProjectApp.swift
+//  Motiv8Project
+//
+//  Created by Sanjana V on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Motiv8ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
